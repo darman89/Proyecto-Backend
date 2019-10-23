851a9723dc753da4d90ba52212a4cd769f53bdca
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-22 21:07:53 -0500

    Actualizacion de los modelos de la app activities de acuerdo al diagrama de clases

--- interactive_content/admin.py
+++ interactive_content/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from interactive_content.models import Contenido, ContenidoInteractivo, Curso, Profesor, Estudiante
-from activities.models import Actividad, Marca, Pregunta
+from activities.models import Actividad, Marca
 
 # Register your models here.
 from interactive_content.models import Curso, Contenido, ContenidoInteractivo, Grupo
