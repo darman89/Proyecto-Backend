0f7d69bf5c70facae9762697d84832cb7ff7e99a
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-25 18:01:57 -0500

    Test listado de calificaciones (ROJO)

--- activities/tests.py
+++ activities/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from rest_framework.utils import json
+from django.contrib.auth.models import User
+import json
 
 # Create your tests here.
+
+class PortafolioTestCase(TestCase):
+    def test_list_calificacion(self):
+        url = '/activities/calificacion'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
