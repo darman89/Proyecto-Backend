4b7b50742c638185d7a5ae6bff44e5388c989d83
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-25 18:11:59 -0500

    Test listado de calificaciones (VERDE)

--- activities/urls.py
+++ activities/urls.py
@@ -1,14 +1,7 @@
-# from activities.views import MarcaView, ActividadView, RespEstudianteMultipleView, PreguntaView, RespMultipleView, \
-#     reports
 from django.urls import path
+from activities import views
+app_name = 'activities'
 
-app_name = 'marca'
-# add url path to the API
 urlpatterns = [
-    # path('marca', MarcaView.as_view(), name='marca'),
-    # path('actividad', ActividadView.as_view(), name='actividad'),
-    # path('pregunta', PreguntaView.as_view(), name='pregunta'),
-    # path('resp_op_multiple', RespMultipleView.as_view(), name='respuesta_op_multiple'),
-    # path('resp_estudiante_op_multiple', RespEstudianteMultipleView.as_view(), name='respuesta_estd_op_multiple'),
-    # path('reports/', reports, name='reports'),
+    path('calificacion', views.list_calificacion, name='calificacion'),
 ]
