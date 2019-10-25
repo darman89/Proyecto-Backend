e2c00f23876c2e0c589eaf1bdc6b9aa085df2bc6
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-25 18:45:52 -0500

    Test conteo de calificaciones (ROJO)

--- activities/tests.py
+++ activities/tests.py
@@ -1,12 +1,44 @@
 from django.test import TestCase
 from rest_framework.utils import json
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 import json
 
+from activities.models import Marca, PreguntaOpcionMultiple, Opcionmultiple, Calificacion
+from users.models import Profesor, Estudiante
+from interactive_content.models import Contenido, ContenidoInteractivo
+
 # Create your tests here.
+
 
 class PortafolioTestCase(TestCase):
     def test_list_calificacion(self):
         url = '/activities/calificacion'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
+
+    def test_count_calificaiones(self):
+        profe = Profesor.objects.create(
+            username='profe12', password='profe123', facultad='Ingenieria')
+        contenido = Contenido.objects.create(
+            url='www.ejemplo.com', nombre='Contenido', profesor=profe)
+        cont_interac = ContenidoInteractivo.objects.create(
+            contenido=contenido, tiene_retroalimentacion=False)
+        marca = Marca.objects.create(
+            nombre='marca', punto=7, contenido=cont_interac)
+        pregunta = PreguntaOpcionMultiple.objects.create(
+            enunciado='Pregunta 1', esMultipleResp=False, nombre='Actividad 1', numeroDeIntentos=3, tieneRetroalimentacion=True, marca=marca)
+        opcion1 = Opcionmultiple.objects.create(
+            opcion='A. Opcion1', esCorrecta=True, preguntaSeleccionMultiple=pregunta)
+        estudiante1 = Estudiante.objects.create(
+            username='esrudiante', password='estudiante123')
+        estudiante2 = Estudiante.objects.create(
+            username='esrudiant2', password='estudiante123')
+        calificacion1 = Calificacion.objects.create(
+            estudiante=estudiante1, actividad=pregunta, calificacion=4.5)
+        calificacion2 = Calificacion.objects.create(
+            estudiante=estudiante2, actividad=pregunta, calificacion=3.5)
+
+        url = '/activities/calificacion'
+        response = self.client.get(url, format='json')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data), 2)
