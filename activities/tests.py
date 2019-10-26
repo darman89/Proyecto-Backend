d8a82678ba7a9e4d80dca9eed8fcd6662731edbb
paolabay <mp.bayonal@uniandes.edu.co>
2019-10-25 19:56:39 -0500

    Status de respuesta de pregunta de seleccion multiple (rojo)

--- activities/tests.py
+++ activities/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import PreguntaOpcionMultiple
+import json
 # Create your tests here.
+
+class PreguntaTestCase(TestCase):
+
+    def test_Get_Pregunta(self):
+        url = '/pregunta/2'
+        response = self.client.get(url, format='json' )
+        self.assertEqual(response.status_code ,200)
