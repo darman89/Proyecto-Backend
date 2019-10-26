961d44c275f21f89867e676706adc27684b23885
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-25 19:15:02 -0500

    Test filtro en la busqueda de calificaciones por estudiante (VERDE)

--- activities/views.py
+++ activities/views.py
@@ -7,7 +7,9 @@ from activities.models import Calificacion
 class CalificarAPI(ListCreateAPIView):
     # serializer usado para la transformacion de datos
     serializer_class = CalificacionSerializer
+    # filtro de estudiante
+    filterset_fields = ("estudiante",)
 
     def get_queryset(self):
-        # queryset para retornar las calificaciones de un estudiante
+        # queryset para retornar las calificaciones de un estudiante        
         return Calificacion.objects.all()
