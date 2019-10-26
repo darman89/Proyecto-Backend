95af673e03b9dec10d152772d95cc9b5903b4a8a
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-25 19:20:10 -0500

    Test de filtro de calificaciones por pregunta (ROJO)

--- activities/tests.py
+++ activities/tests.py
@@ -69,3 +69,34 @@ class PortafolioTestCase(TestCase):
         response = self.client.get(url, format='json')
         current_data = json.loads(response.content)
         self.assertEqual(len(current_data), 1)
+
+    def test_filter_calificaiones_by_question(self):
+        profe = Profesor.objects.create(
+            username='profe12', password='profe123', facultad='Ingenieria')
+        contenido = Contenido.objects.create(
+            url='www.ejemplo.com', nombre='Contenido', profesor=profe)
+        cont_interac = ContenidoInteractivo.objects.create(
+            contenido=contenido, tiene_retroalimentacion=False)
+        marca = Marca.objects.create(
+            nombre='marca', punto=7, contenido=cont_interac)
+        pregunta = PreguntaOpcionMultiple.objects.create(
+            enunciado='Pregunta 1', esMultipleResp=False, nombre='Actividad 1', numeroDeIntentos=3, tieneRetroalimentacion=True, marca=marca)
+        pregunta2 = PreguntaOpcionMultiple.objects.create(
+            enunciado='Pregunta 2', esMultipleResp=False, nombre='Actividad 2', numeroDeIntentos=1, tieneRetroalimentacion=True, marca=marca)
+        opcion1 = Opcionmultiple.objects.create(
+            opcion='A. Opcion1', esCorrecta=True, preguntaSeleccionMultiple=pregunta)
+        estudiante1 = Estudiante.objects.create(
+            username='esrudiante', password='estudiante123')
+        estudiante2 = Estudiante.objects.create(
+            username='esrudiant2', password='estudiante123')
+        calificacion1 = Calificacion.objects.create(
+            estudiante=estudiante1, actividad=pregunta, calificacion=4.5)
+        calificacion2 = Calificacion.objects.create(
+            estudiante=estudiante2, actividad=pregunta, calificacion=3.5)
+        calificacion3 = Calificacion.objects.create(
+            estudiante=estudiante1, actividad=pregunta2, calificacion=5.0)
+
+        url = '/activities/calificacion?pregunta=1'
+        response = self.client.get(url, format='json')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data), 2)
