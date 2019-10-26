a928d83ab190be9f25c512c64921008abdf0d9a1
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-25 19:03:38 -0500

    Test filtro en la busqueda de calificaciones por estudiante (ROJO)

--- activities/tests.py
+++ activities/tests.py
@@ -42,3 +42,30 @@ class PortafolioTestCase(TestCase):
         response = self.client.get(url, format='json')
         current_data = json.loads(response.content)
         self.assertEqual(len(current_data), 2)
+
+    def test_filter_calificaiones_by_student(self):
+        profe = Profesor.objects.create(
+            username='profe12', password='profe123', facultad='Ingenieria')
+        contenido = Contenido.objects.create(
+            url='www.ejemplo.com', nombre='Contenido', profesor=profe)
+        cont_interac = ContenidoInteractivo.objects.create(
+            contenido=contenido, tiene_retroalimentacion=False)
+        marca = Marca.objects.create(
+            nombre='marca', punto=7, contenido=cont_interac)
+        pregunta = PreguntaOpcionMultiple.objects.create(
+            enunciado='Pregunta 1', esMultipleResp=False, nombre='Actividad 1', numeroDeIntentos=3, tieneRetroalimentacion=True, marca=marca)
+        opcion1 = Opcionmultiple.objects.create(
+            opcion='A. Opcion1', esCorrecta=True, preguntaSeleccionMultiple=pregunta)
+        estudiante1 = Estudiante.objects.create(
+            username='esrudiante', password='estudiante123')
+        estudiante2 = Estudiante.objects.create(
+            username='esrudiant2', password='estudiante123')
+        calificacion1 = Calificacion.objects.create(
+            estudiante=estudiante1, actividad=pregunta, calificacion=4.5)
+        calificacion2 = Calificacion.objects.create(
+            estudiante=estudiante2, actividad=pregunta, calificacion=3.5)
+
+        url = '/activities/calificacion?estudiante=1'
+        response = self.client.get(url, format='json')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data), 1)
