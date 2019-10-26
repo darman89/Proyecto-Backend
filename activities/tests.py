dc191eea288e27be402e998f2fd56b16d85ad49d
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-26 00:48:49 -0500

    Test creacion de metodo POST para creacion de calificaciones (VERDE)

--- activities/tests.py
+++ activities/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.utils import json
 from django.contrib.auth.models import User, AbstractUser
 import json
+from django.http import JsonResponse
 
 from activities.models import Marca, PreguntaOpcionMultiple, Opcionmultiple, Calificacion
 from users.models import Profesor, Estudiante
@@ -96,10 +97,10 @@ class PortafolioTestCase(TestCase):
         calificacion3 = Calificacion.objects.create(
             estudiante=estudiante1, actividad=pregunta2, calificacion=5.0)
 
-        url = '/activities/calificacion?actividad=2'
+        url = '/activities/calificacion?actividad=1'
         response = self.client.get(url, format='json')
         current_data = json.loads(response.content)
-        self.assertEqual(len(current_data), 2)
+        self.assertEqual(len(current_data), 1)
 
     def test_filter_obligatory(self):
         profe = Profesor.objects.create(
@@ -131,3 +132,31 @@ class PortafolioTestCase(TestCase):
         response = self.client.get(url, format='json')
         current_data = json.loads(response.content)
         self.assertEqual(len(current_data), 0)
+
+    def test_create_calificacion(self):
+        profe = Profesor.objects.create(
+            username='profe12', password='profe123', facultad='Ingenieria')
+        contenido = Contenido.objects.create(
+            url='www.ejemplo.com', nombre='Contenido', profesor=profe)
+        cont_interac = ContenidoInteractivo.objects.create(
+            contenido=contenido, tiene_retroalimentacion=False)
+        marca = Marca.objects.create(
+            nombre='marca', punto=7, contenido=cont_interac)
+        pregunta = PreguntaOpcionMultiple.objects.create(
+            enunciado='Pregunta 1', esMultipleResp=False, nombre='Actividad 1', numeroDeIntentos=3, tieneRetroalimentacion=True, marca=marca)
+        opcion1 = Opcionmultiple.objects.create(
+            opcion='A. Opcion1', esCorrecta=True, preguntaSeleccionMultiple=pregunta)
+        estudiante1 = Estudiante.objects.create(
+            username='esrudiante', password='estudiante123')
+        estudiante2 = Estudiante.objects.create(
+            username='esrudiante2', password='estudiante123')
+
+        url = '/activities/calificacion'
+        data = JsonResponse(
+            {"estudiante": "1", "actividad": "1", "calificacion": "3.7"})
+        self.client.post(url, data)
+
+        url = '/activities/calificacion?actividad=1'
+        response = self.client.get(url, format='json')
+        current_data = json.loads(response.content)
+        self.assertEqual(len(current_data), 1)
