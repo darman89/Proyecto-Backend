2d1584acc86936665b56aa899979b4aa891e07d5
Juan Camilo C <j.cardenasc@uniandes.edu.co>
2019-10-26 16:52:08 -0500

    Se agrega la validacion del numero de intentos permitidos antes de guardar ua respuesta

--- activities/views.py
+++ activities/views.py
@@ -1,57 +1,35 @@
 
-from rest_framework.generics import GenericAPIView,ListCreateAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework import generics, permissions, serializers, viewsets
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
 
 from activities.serializers import PreguntaSeleccionMultipleSerializer, CalificacionSerializer, RespuestaSeleccionMultipleSerializer
-from activities.models import Calificacion, PreguntaOpcionMultiple
+from activities.models import Calificacion, PreguntaOpcionMultiple, RespuestmultipleEstudiante, Opcionmultiple
+
 
 class DetailPreguntaSeleccionMultiple(generics.RetrieveUpdateDestroyAPIView):
     queryset = PreguntaOpcionMultiple.objects.all()
     serializer_class = PreguntaSeleccionMultipleSerializer
 
+
 class PreguntaView(ListModelMixin, CreateModelMixin, GenericAPIView):
-     # Add permissions to the view
-     # permission_classes = [IsAuthenticated]
-
-     # Add filter fields for the API
-     filterset_fields = ("actividad",)
-     # clase serializer para la transformacion de datos del request
-     serializer_class = PreguntaSeleccionMultipleSerializer
-
-     #def get_queryset(self):
-         #actividad = self.request.query_params.get('actividad')
-         #return PreguntaOpcionMultiple.objects.filter(actividad=actividad)
-
-     def perform_create(self, serializer):
-         #actividad = get_object_or_404(
-         #    Actividad, id=self.request.data.get('actividad'))
-         return serializer.save()
-
-     def get(self, request, *args, **kwargs):
-         return self.list(request, *args, *kwargs)
-
-     def post(self, request, *args, **kwargs):
-         return self.create(request, *args, **kwargs)
-
-
-class RespuestaSeleccionMultipleView(ListModelMixin, CreateModelMixin, GenericAPIView):
     # Add permissions to the view
     # permission_classes = [IsAuthenticated]
 
-
+    # Add filter fields for the API
+    filterset_fields = ("actividad",)
     # clase serializer para la transformacion de datos del request
-    serializer_class = RespuestaSeleccionMultipleSerializer
+    serializer_class = PreguntaSeleccionMultipleSerializer
 
     # def get_queryset(self):
-    # actividad = self.request.query_params.get('actividad')
+    #actividad = self.request.query_params.get('actividad')
     # return PreguntaOpcionMultiple.objects.filter(actividad=actividad)
 
     def perform_create(self, serializer):
-
         # actividad = get_object_or_404(
         #    Actividad, id=self.request.data.get('actividad'))
         return serializer.save()
@@ -61,7 +39,37 @@ class RespuestaSeleccionMultipleView(ListModelMixin, CreateModelMixin, GenericAP
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-      
+
+
+class RespuestaSeleccionMultipleView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    # Add permissions to the view
+    # permission_classes = [IsAuthenticated]
+
+    queryset = RespuestmultipleEstudiante.objects.all()
+    # clase serializer para la transformacion de datos del request
+    serializer_class = RespuestaSeleccionMultipleSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, *kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        opcion = Opcionmultiple.objects.filter(
+            id=self.request.data['respuestmultiple'])
+        pregunta = opcion[0].preguntaSeleccionMultiple
+        # valida si el intento de la respuesta es menor o igual al max de intentos permitidos
+        if self.request.data['intento'] <= pregunta.numeroDeIntentos:
+            serializer = self.get_serializer(data=request.data)
+            return serializer.save()
+        else:
+            msj = {'max_attemps': 'Número de intentos maximos excedido'}
+            return Response(msj, status=status.HTTP_406_NOT_ACCEPTABLE)
+
 
 class CalificarAPI(ListCreateAPIView):
     # Add filter fields for the API
@@ -77,5 +85,3 @@ class CalificarAPI(ListCreateAPIView):
             return Calificacion.objects.filter(estudiante=student)
         if (activity):
             return Calificacion.objects.filter(actividad=activity)
-
-
