8718aae6f38f9f1c3a45b4006e1e4298aa681c1f
Catalina <cata.cordoba8@gmail.com>
2019-10-27 18:58:53 -0500

    Revert "Obtener Lista OK: Rojo"
    This reverts commit 0acf6b34cee257c196d9d165e6c409e7e124dbb7.
    
    # Conflicts:
    #	interactive_content/tests.py
    #	interactive_content/urls.py

--- interactive_content/tests.py
+++ interactive_content/tests.py
@@ -1,30 +1,3 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework.authtoken.models import Token
-from rest_framework.utils import json
 
-from interactive_content.models import Contenido
-from users.models import Profesor
-
-
-class InteractiveContentTestCase(TestCase):
-
-    def setUp(self):
-        self.client = APIClient()
-        self.user = Profesor.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
-        self.token = Token.objects.create(user=self.user)
-
-    def test_lista_contenido_ok(self):
-        url = '/content/content/'
-        self.client.force_login(user=self.user)
-        response = self.client.get(url, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
-        self.assertEqual(response.status_code, 200)
-
-    def test_lista_contenido_un_elemento(self):
-        url = '/content/content/'
-        self.client.force_login(user=self.user)
-        content = Contenido.objects.create(nombre="Contenido Prueba 1", url="https://youtube/test", profesor=self.user)
-        response = self.client.get(url, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
-        current_data = json.loads(response.content)
-        self.assertEqual(len(current_data), 1)
-        self.assertEqual(current_data[0]['nombre'], content.nombre)
+# Create your tests here.
