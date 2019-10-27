0acf6b34cee257c196d9d165e6c409e7e124dbb7
Catalina <cata.cordoba8@gmail.com>
2019-10-27 18:08:21 -0500

    Obtener Lista OK: Rojo

--- interactive_content/tests.py
+++ interactive_content/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from users.models import Profesor
 
-# Create your tests here.
+
+class InteractiveContentTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = Profesor.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
+        self.token = Token.objects.create(user=self.user)
+
+    def test_lista_contenido_ok(self):
+        url = '/content/content/'
+        self.client.force_login(user=self.user)
+        response = self.client.get(url, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.assertEqual(response.status_code, 200)
