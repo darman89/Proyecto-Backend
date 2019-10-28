20e143038da8937687b5f6bf66377a0a35cae20b
carlos <cdaza933@hotmail.com>
2019-10-27 19:36:09 -0500

    add swagger

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('content/', include('interactive_content.urls')),
