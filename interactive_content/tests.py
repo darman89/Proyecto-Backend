7b10bab338187d4adf90cdbf0ba0e0db02941099
carlos daza <carlos.daza@alkanza.us>
2019-11-06 08:26:27 -0500

    fix test cases

--- interactive_content/tests.py
+++ interactive_content/tests.py
@@ -40,12 +40,17 @@ class CreateInteractiveContentTestCase(TestCase):
         current_data = json.loads(response.content)
         self.assertEqual(current_data['message'], 'Unauthorized')
         self.assertEqual(response.status_code, 401)
+
+
 class InteractiveContentTestCase(TestCase):
 
     def setUp(self):
         self.client = APIClient()
         self.user = Profesor.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
         self.token = Token.objects.create(user=self.user)
+        self.url = '/content/interactivecontent/1'
+        self.headers = {'Authorization': 'Token {}'.format(self.token.key),
+                        'Content-Type': 'application/json'}
 
     def test_add_interactive_content(self):
         url = '/content/cont_interactivo'
@@ -71,46 +76,8 @@ class InteractiveContentTestCase(TestCase):
         self.assertEqual(current_data['message'], 'Unauthorized')
         self.assertEqual(response.status_code, 401)
 
-
-class InteractiveContentTestCase(TestCase):
-
-    def setUp(self):
-        self.client = APIClient()
-        self.user = Profesor.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
-        self.token = Token.objects.create(user=self.user)
-
-    def test_lista_contenido_ok(self):
-        url = '/content/content/'
-        self.client.force_login(user=self.user)
-        response = self.client.get(url, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
-        self.assertEqual(response.status_code, 200)
-
-    def test_lista_contenido_un_elemento(self):
-        url = '/content/content/'
-        self.client.force_login(user=self.user)
-        content = Contenido.objects.create(nombre="Contenido Prueba 1", url="https://youtube/test", profesor=self.user)
-        response = self.client.get(url, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
-        current_data = json.loads(response.content)
-        self.assertEqual(len(current_data), 1)
-        self.assertEqual(current_data[0]['nombre'], content.nombre)
-
-    def test_lista_contenido_dos_elementos(self):
-        url = '/content/content/'
-        self.client.force_login(user=self.user)
-        content_1 = Contenido.objects.create(nombre="Contenido Prueba 2", url="https://youtube/test2",
-                                             profesor=self.user)
-        content_2 = Contenido.objects.create(nombre="Contenido Prueba 3", url="https://youtube/test3",
-                                             profesor=self.user)
-        response = self.client.get(url, format='json', HTTP_AUTHORIZATION='Token ' + self.token.key)
-        current_data = json.loads(response.content)
-        self.assertEqual(len(current_data), 2)
-        print(current_data)
-        self.assertEqual(current_data[0]['nombre'], content_2.nombre)
-        self.assertEqual(current_data[1]['nombre'], content_1.nombre)
-    url = '/content/interactivecontent/1'
-    headers = {'Authorization': 'Token 452c39042e5e9874be461bbab2437a34a249014b', 'Content-Type': 'application/json'}
-
     def test_get_interactive_content_200_status(self):
+
         response = self.client.get(self.url, format='json', headers=self.headers)
         self.assertEqual(response.status_code, 200)
 
