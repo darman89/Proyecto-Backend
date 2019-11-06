89e076b87a3bb6a7283bc47cb1aa8606b02f7ab8
Armando Acosta Guerrero <darman.89@gmail.com>
2019-11-05 23:03:56 -0500

    Creación de Contenido Interactivo por Usuario no Autorizado (ROJO)

--- interactive_content/tests.py
+++ interactive_content/tests.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
 from interactive_content.models import Contenido
-from users.models import Profesor
+from users.models import Profesor, Estudiante
 
 
 # Create your tests here.
@@ -26,3 +26,16 @@ class CreateInteractiveContentTestCase(TestCase):
         current_data = json.loads(response.content)
         self.assertEqual(current_data['nombre'], 'test')
         self.assertEqual(current_data['contenido']['id'], contenido.id)
+
+    def test_unauthorized_user(self):
+        url = '/content/cont_interactivo'
+        estudiante = Estudiante.objects.create_user('estudiante', 'estudiante@admin.com', 'estudiante123')
+        interactive_content = {"nombre": "test", "contenido": "1"}
+        self.token = Token.objects.create(user=estudiante)
+        self.client.force_login(user=estudiante)
+        Contenido.objects.create(url="test.com", nombre="contenido test", profesor_id=self.user.id)
+        response = self.client.post(url, interactive_content, format='json',
+                                    HTTP_AUTHORIZATION='Token ' + self.token.key)
+        current_data = json.loads(response.content)
+        self.assertEqual(current_data['message'], 'Unauthorized')
+        self.assertEqual(response.status_code, 401)
